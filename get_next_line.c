dd18cca4d532697dc61929fb7d1f8c484d616c82
Shemika Powers <spowers@student.21-school.ru>
2020-07-18 21:32:09 +0300

    f

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: spowers <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/16 23:28:47 by spowers           #+#    #+#             */
-/*   Updated: 2020/07/18 18:31:59 by spowers          ###   ########.fr       */
+/*   Updated: 2020/07/18 21:31:18 by spowers          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void			ft_free(char **str)
+void		ft_free(char **str)
 {
 	if (str != NULL && *str != NULL)
 		free(*str);
@@ -36,34 +36,34 @@ int		get_line(char *str, char **line, int i)
 	int		len;
 
 	if (!(*line = ft_substr(str, 0, i)))
-	return (-1);
+		return (-1);
 	++i;
 	len = ft_strlen(str + i) + 1;
 	ft_memcpy(str, str + i, len);
 	return (1);
 }
 
-int		set_line(char *str, char **line, int bwr)
+int		set_line(char **str, char **line, int bwr)
 {
-	if (str)
-        {
-               if (!(*line = ft_strdup(str)))
-                return(-1);
-                ft_free(&str);
-                return (bwr);
-        }
-        if (!(*line = ft_strdup("")))
-		return(-1);
+	if (*str)
+	{
+		if (!(*line = ft_strdup(*str)))
+			return (-1);
+		ft_free(str);
+		return (bwr);
+	}
+	if (!(*line = ft_strdup("")))
+		return (-1);
 	return (bwr);
 }
 
 int		get_next_line(int fd, char **line)
 {
-	char			buff[BUFFER_SIZE + 1];
-	static char		*str;
-	int				bwr;
-	int				i;
-	
+	char		buff[BUFFER_SIZE + 1];
+	static char	*str;
+	int		bwr;
+	int		i;
+
 	if (!line || fd < 0 || BUFFER_SIZE < 1 || read(fd, buff, 0) < 0)
 		return (-1);
 	if (str && (((i = check_index(str, '\n')) != -1)))
@@ -76,5 +76,5 @@ int		get_next_line(int fd, char **line)
 		if (((i = check_index(str, '\n')) != -1))
 			return (get_line(str, line, i));
 	}
-	return (set_line(str, line, bwr));
+	return (set_line(&str, line, bwr));
 }
